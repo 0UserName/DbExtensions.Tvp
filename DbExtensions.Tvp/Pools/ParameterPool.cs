b5ede99ab0b05809c41b66d49b141cb99ec52f7e
0username <net.fedorax64@gmail.com>
2026-09-01 05:17:18 +0300

    redundant comment removed

--- DbExtensions.Tvp/Pools/ParameterPool.cs
+++ DbExtensions.Tvp/Pools/ParameterPool.cs
@@ -4,9 +4,6 @@ namespace DbExtensions.Tvp.Pools
 {
     internal sealed class ParameterPool<TParameter> : DefaultObjectPool<TParameter> where TParameter : class, IResettable
     {
-        /// <summary>
-        /// Retrieves a shared <see cref="ParameterPool{TParameter}"/> instance.
-        /// </summary>
         public static readonly
             ParameterPool<TParameter> Shared = new
             ParameterPool<TParameter>
