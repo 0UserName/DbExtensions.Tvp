8ed2d48deb3afd8627866cd6f38ea2b153de15db
0username <net.fedorax64@gmail.com>
2026-09-07 00:48:54 +0300

    Begin/EndInit calls removed from DataTableExtensions due to redundancy

--- DbExtensions.Tvp/Parameters/DataTableExtensions.cs
+++ DbExtensions.Tvp/Parameters/DataTableExtensions.cs
@@ -13,16 +13,12 @@ namespace DbExtensions.Tvp.Parameters
         /// </remarks>
         public static DataTable InitColumns<TRow>(this DataTable table) where TRow : ITableValued
         {
-            table.BeginInit();
-
             foreach (IColumnInternalMetadata internalMetadata in TRow.Metadata.Columns)
             {
                 table.Columns.Add(internalMetadata is IColumnExternalMetadata externalMetadata ?
                     new DataTableParameterColumn(externalMetadata.AllowDBNull, externalMetadata.Name, externalMetadata.Type, externalMetadata.MaxLength, externalMetadata.Unique) :
                     new DataTableParameterColumn(internalMetadata.AllowDBNull, internalMetadata.Name, internalMetadata.Type));
             }
-
-            table.EndInit();
 
             return table;
         }
